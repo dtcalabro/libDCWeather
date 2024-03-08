09b45f13c065dc998addae15ffee6edf3632582c
Derek Calabro <45155421+dtcalabro@users.noreply.github.com>
2024-03-08 11:53:05 -0500

    Added public header

---
+++ public-headers/libDCWeather.h
@@ -0,0 +1,15 @@
+// public libDCWeather.h
+#import <Foundation/Foundation.h>
+
+@interface libDCWeather : NSObject
++ (instancetype)sharedInstance;
+- (void)requestRefresh;
+- (void)setAutoUpdateInvervalInSeconds:(NSInteger)interval;
+- (void)setAutoUpdateInvervalInMinutes:(NSInteger)interval;
+- (void)setAutoUpdateInvervalInHours:(NSInteger)interval;
+- (void)conditionIncludesSevereWeather:(BOOL)conditionIncludesSevereWeather;
+- (NSString *)temperatureString;
+- (NSString *)conditionString;
+- (UIImage *)conditionImage;
+- (NSString *)cityString;
+@end
