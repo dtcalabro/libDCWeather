0b125316c49911bd514ad3faac39572716a8af84
Derek Calabro <45155421+dtcalabro@users.noreply.github.com>
2024-04-04 19:39:02 -0400

    Add support for theos rootful install

--- install_to_theos.sh
+++ install_to_theos.sh
@@ -5,6 +5,9 @@ set -e
 ./build.sh rootless final
 cp -Rv "./.theos/obj/libDCWeather.dylib" "$THEOS/lib/iphone/rootless"
 
+./build.sh final
+cp -Rv "./.theos/obj/libDCWeather.dylib" "$THEOS/lib/iphone"
+
 cp -v "./public-headers/libDCWeather.h" "$THEOS/include"
 
 echo "Successfully installed libDCWeather"
